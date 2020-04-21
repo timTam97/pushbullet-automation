5dc503980eb858a2620ad32715d8f8a98ca71660
Timothy Samraj <tsam0005@student.monash.edu>
2020-04-21 11:05:11 +1000

    Add file

---
+++ wss.py
@@ -0,0 +1,61 @@
+from api_key import get_key
+import requests
+import json
+import time
+import websocket
+import time
+import subprocess
+
+def on_message(ws, message):
+    print("message")
+    print(message)
+    content = json.loads(message)
+    sub = content.get("subtype")
+    if sub == "push":
+        response = grab_push()
+        title = response[0]
+        body = response[1]
+        if title == "Push2Run LENOVO-TIM" and body is not None:
+            process_command(body)
+
+def grab_push():
+    url = "https://api.pushbullet.com/v2/pushes"
+    headers = {
+    "Access-Token":get_key(),
+    }
+    params = {
+    "active":"true",
+    "modified_after":int(time.time()) - 60
+    }
+    r = requests.get(url, headers=headers, params=params).json()
+    return r.get('pushes')[0].get('title'), r.get('pushes')[0].get('body')
+
+def process_command(command):
+    if command == "sleep":
+        subprocess.run(["systemctl", "suspend"])
+    elif command == "hibernate":
+        pass # idk how to hibernate on linux
+    elif command == "shut down":
+        subprocess.run(["poweroff"]) # untested
+
+
+def on_error(ws, error):
+    print("error")
+    print(error)
+
+def on_close(ws):
+    print("### closed ###")
+
+def on_open(ws):
+    print("open")
+
+
+if __name__ == "__main__":
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("wss://stream.pushbullet.com/websocket/" + get_key(),
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
+    # process_command("sleep")
